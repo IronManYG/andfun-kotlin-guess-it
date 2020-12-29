4a6c0c7c79ce2203780464e7ba31f4ab57f73948
IronManYG <hussain.yg2010@gmail.com>
2020-12-29 13:57:17 +0300

    LiveData Map Transformation

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,8 +17,12 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.view.animation.Transformation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.ViewModel
 
 
@@ -47,9 +51,13 @@ class GameViewModel : ViewModel() {
     val currentTime: LiveData<Long>
         get() = _currentTime
 
-    // TODO (01) Create a new LiveData called currentTimeString.
-    // TODO (02) Use Transformation.map to take the number output from currentTime, and transform
+    //  (01) Create a new LiveData called currentTimeString.
+    //  (02) Use Transformation.map to take the number output from currentTime, and transform
     // it into a String using DateUtils.
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
 
     // The current word
     private val _word = MutableLiveData<String>()
